2379825ebe15331347868e985f510f6c55417fce
HelmiRafif <helmi.rafif@gmail.com>
2023-03-13 21:11:31 +0700

    Menambahkan Excercise1.py

---
+++ Excercise1.py
@@ -0,0 +1,11 @@
+nama = ("Helmi Rafif Fauzan")
+NIM = ("I0322052")
+Asal = ("Saya berasal dari Jakarta Selatan")
+Visual_asal = ("walaupun saya tinggal di Jakarta Selatan, jakarta yang saya tempati berada di pinggiran mepet dengan tanggerang.hidup dipinggiran jakarta mampu membuat saya membandingkan bagaimana ketimpangan sosial. orang yang berkecukupan dengan yang tidak. ditempat saya tinggal ketimpangan tersebut dapat terlihat jelas namun masyarakat disana hidup berdampingan, tanpa  berselisih. Jakarta yang di katakan penuh polusi, tidak begitu tampak di tempat saya karena wilayah pinggir jakarta dapat berkembang dengan keasrian alamnya")
+harapan = ("Harapan saya dengan berkuliah di TI UNS dapat menguasai banyak hal dan mampu menjadi nomor satu sehingga dapat bersaing dalam dunia pernafkahan")
+
+print (nama)
+print (NIM)
+print (Asal)
+print (Visual_asal)
+print (harapan)
